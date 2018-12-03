1036db0d5096084391b5db22c3557f13772b28b1
DanielDonato <daniel_donato10@yahoo.com.br>
2018-12-02 23:52:49 -0200

    Creating findById

--- users/users.model.ts
+++ users/users.model.ts
@@ -1,10 +1,20 @@
 const users = [
-    {name: 'Petter Parker', email: 'petter@marvel.com'},
-    {name: 'Bruce Wayne', email: 'bruce@marvel.com'},
+    {id: '1', name: 'Petter Parker', email: 'petter@marvel.com'},
+    {id: '2', name: 'Bruce Wayne', email: 'bruce@marvel.com'},
 ]
 
 export class User{
     static findAll():Promise<any>{
         return Promise.resolve(users);
+    }
+    static findById(id:string):Promise<any>{
+        return new Promise(resolve => {
+            const filtred = users.filter(user => user.id === id);
+            let user = undefined
+            if(filtred.length > 0){
+                user = filtred[0]
+            }
+            resolve(user);
+        })
     }
 }
