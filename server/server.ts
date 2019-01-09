e151809141f670702c38af99b35569ecd5b793e2
DanielDonato <daniel_donato10@yahoo.com.br>
2019-01-09 16:24:03 -0200

    Adding mongoose and connect with db

--- server/server.ts
+++ server/server.ts
@@ -1,22 +1,29 @@
 import * as restify from 'restify';
 import {enviroment} from '../cummon/enviroment';
 import {Router} from '../cummon/router';
+import * as mongoose from 'mongoose';
 
 export class Server {
 
     application : restify.Server
 
+    initializeDb(): mongoose.MongooseThenable {
+        (<any>mongoose).Promise = global.Promise;
+        return mongoose.connect(enviroment.db.url, {
+            useMongoClient: true
+        });
+    }
+
     initRoutes(routers: Router[]): Promise<any>{
         return new Promise( (resolve, reject) => {
             try{
-                
                 this.application = restify.createServer({
                     name : "meat-api",
                     version : "1.0.0"
                 });
                 
                 this.application.use(restify.plugins.queryParser());
-                this.application.use(restify.plugins.bodyParser());
+                //this.application.use(restify.plugins.bodyParser());
 
                 //routes
                 for(let router of routers){
@@ -34,6 +41,6 @@ export class Server {
     }
     
     bootstrap(routers: Router[] = []): Promise<Server>{
-        return this.initRoutes(routers).then(() => this);
+        return this.initializeDb().then(() => this.initRoutes(routers).then(() => this));
     }
 }
