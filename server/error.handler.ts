e8c5f33ce6508e128fdaa0dbd745cd998dfe0020
DanielDonato <daniel_donato10@yahoo.com.br>
2019-01-20 16:04:53 -0200

    Default validators

--- server/error.handler.ts
+++ server/error.handler.ts
@@ -3,7 +3,7 @@ import * as restify from 'restify';
 export const handleError = (req:restify.Request, res:restify.Response, err, done) => {
     err.toJSON = () => {
         return {
-            message: err.message
+            message: err.message,
         }
     }
     switch(err.name){
@@ -12,8 +12,15 @@ export const handleError = (req:restify.Request, res:restify.Response, err, done
                 err.statusCode = 400;
             }
             break;
-        case 'ValidationErro':
+        case 'ValidationError':
         err.statusCode = 400;
+        const messages: any[] = [];
+        for(let name in err.errors){
+            messages.push({message: err.errors[name].message});
+        }
+        err.toJSON = () => ({
+            errors: messages
+        })
         break;
     }
     done()
