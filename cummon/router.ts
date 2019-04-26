57a03209cd83ad4663fad2f45cc437ec2a898a8b
DanielDonato <daniel_donato10@yahoo.com.br>
2019-04-25 23:07:26 -0300

    Adding links

--- cummon/router.ts
+++ cummon/router.ts
@@ -5,11 +5,15 @@ import {NotFoundError} from 'restify-errors';
 export abstract class Router extends EventEmitter {
     abstract applyRoutes(application: restify.Server);
 
+    envelope(document: any): any{
+        return document;
+    }
+
     render(res: restify.Response, next :restify.Next){
         return (document) => {
             if(document){
                 this.emit('beforeRender', document);
-                res.json(document);
+                res.json(this.envelope(document));
             }else {
                 throw new NotFoundError('Document not found');
             }
@@ -20,13 +24,15 @@ export abstract class Router extends EventEmitter {
     renderAll(res: restify.Response, next :restify.Next){
         return (documents: any[]) => {
             if(documents){
-                documents.forEach(document => {
+                documents.forEach((document, index, array) => {
                     this.emit('beforeRender', document);
+                    array[index] = this.envelope(document);
                 });
                 res.json(documents);
             } else {
                 res.json([]);
             }
+            return next();
         }
     }
 
--- reviews/reviews.router.ts
+++ reviews/reviews.router.ts
@@ -19,6 +19,13 @@ class ReviewsRouter extends ModelRouter<Review> {
             .catch(next);
     };*/
 
+    envelope(document){
+        let resource = super.envelope(document);
+        const restId = document.restaurant._id ? document.restaurant._id : document.restaurant;
+        resource._links.restaurant = `/restaurants/${restId}`;
+        return resource;
+    }
+
     //overide
     protected prepareOne(query: mongoose.DocumentQuery<Review, Review>): mongoose.DocumentQuery<Review, Review>{
         return query
@@ -27,12 +34,12 @@ class ReviewsRouter extends ModelRouter<Review> {
     }
 
     applyRoutes(application: restify.Server){
-        application.get('/reviews', this.findAll);
-        application.get('/reviews/:id', [this.validateId, this.findById]);
-        application.post('/reviews', this.save);
-        application.put('/reviews/:id', [this.validateId,this.replace]);
-        application.patch('/reviews/:id', [this.validateId,this.update]);
-        application.del('/reviews/:id', [this.validateId,this.delete]);
+        application.get(`/${this.basePath}`, this.findAll);
+        application.get(`/${this.basePath}/:id`, [this.validateId, this.findById]);
+        application.post(`/${this.basePath}`, this.save);
+        application.put(`/${this.basePath}/:id`, [this.validateId,this.replace]);
+        application.patch(`/${this.basePath}/:id`, [this.validateId,this.update]);
+        application.del(`/${this.basePath}/:id`, [this.validateId,this.delete]);
     }
 }
 
